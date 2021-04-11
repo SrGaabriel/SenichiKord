03461a82bbd124d23ab7d3faf2685c07758ec86a
xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>
2021-04-11 11:51:47 -0300

    Also refactor ping system

--- src/main/kotlin/me/hechfx/project/command/vanilla/misc/PingCommand.kt
+++ src/main/kotlin/me/hechfx/project/command/vanilla/misc/PingCommand.kt
@@ -1,16 +1,17 @@
 package me.hechfx.project.command.vanilla.misc
 
 import dev.kord.core.behavior.edit
-import me.hechfx.project.api.Command
+import me.hechfx.project.api.TextCommand
 import me.hechfx.project.api.CommandContext
 import me.hechfx.project.util.CommandCategory
 
-class PingCommand() : Command {
-    override val labels = listOf("ping", "latency", "latencia")
-    override val category = CommandCategory.MISCELLANEOUS
-    override val description = "Ping pong!"
-    override val onlyDev = false
-    override val debugMode = false
+class PingCommand() : TextCommand(Options) {
+    companion object Options: TextCommand.Options("ping", "latency", "latencia") {
+        override var category = CommandCategory.MISCELLANEOUS
+        override var description = "Ping pong!"
+        override val onlyDev = false
+        override val debugMode = false
+    }
 
     override suspend fun run(context: CommandContext) {
         val time = System.currentTimeMillis()
